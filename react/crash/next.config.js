da87ba5cc46d69907e9497697b2416758162f9b3
sitaar <zhumengnian123@outlook.com>
2023-03-24 15:09:01 +0800

    test

--- react/crash/next.config.js
+++ react/crash/next.config.js
@@ -11,17 +11,17 @@ module.exports = {
   experimental: {
     appDir: true,
   },
-  output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "build.[contenthash:10].js",
-    publicPath: "/",
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://way.jd.com/:path*'
-      }
-    ]
-  }
+  // output: {
+  //   path: path.resolve(__dirname, "build"),
+  //   filename: "build.[contenthash:10].js",
+  //   publicPath: "/",
+  // },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api/:path*',
+  //       destination: 'https://way.jd.com/:path*'
+  //     }
+  //   ]
+  // }
 }
